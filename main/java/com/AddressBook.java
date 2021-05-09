61cedc8b5b1cb4a690e76affe11db99f395996f2
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-05-10 00:19:52 +0530

    [Vaishnavi]ADDED : Editing the contact details in address book

--- main/java/com/AddressBook.java
+++ main/java/com/AddressBook.java
@@ -10,22 +10,62 @@ public class AddressBook {
 
     /*Adding Welcome Message
     *Creating Contact class to creating contact in addressbook*
-    *Adding Contacts in Address Book*/
+    *Adding Contacts in Address Book
+    * Editing contact details in Address Book*/
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
     }
 
-    public boolean addContact(Contact contact) {
+    public List<Contact> addContact(Contact contact) {
         System.out.println(contact);
         List<Contact> contactList = new ArrayList<>();
         try {
             contactList.add(contact);
             System.out.println(contactList);
-            return true;
+            return contactList;
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return false;
+        return contactList;
+    }
+
+    public Contact editContact(List<Contact> contactList, String name, String fieldName, String edit) {
+        try {
+            for(Contact contact : contactList){
+                if(contact.firstName.equals(name)) {
+                    switch(fieldName) {
+                        case "firstName":
+                            contact.firstName = edit;
+                            break;
+                        case "lastName":
+                            contact.lastName = edit;
+                            break;
+                        case "address":
+                            contact.address = edit;
+                            break;
+                        case "city":
+                            contact.city = edit;
+                            break;
+                        case "state":
+                            contact.state = edit;
+                            break;
+                        case "zip":
+                            contact.zip = Integer.parseInt(edit);
+                            break;
+                        case "phone":
+                            contact.phone = Long.parseLong(edit);
+                            break;
+                        case "email":
+                            contact.email = edit;
+                            break;
+                    }
+                }
+                return contact;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
 
--- test/java/com/AddressBookTest.java
+++ test/java/com/AddressBookTest.java
@@ -3,6 +3,9 @@ package com;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.function.BooleanSupplier;
+
 public class AddressBookTest {
 
     public AddressBook addressBook;
@@ -11,15 +14,33 @@ public class AddressBookTest {
     public void givenContact_WhenAddedToAddressBook_ShouldReturn_true(){
         AddressBook addressBookMain = new AddressBook();
         Contact contact = new Contact("SIYA", "Kapoor", "IUDP", "Amravati", "MH", 444601, 952772864, "siyas@gmail.com");
-        boolean isadded = addressBookMain.addContact(contact);
-        Assertions.assertTrue(isadded);
+        List isadded = addressBookMain.addContact(contact);
+        Assertions.assertTrue((BooleanSupplier) isadded);
     }
 
     @Test
     public void givenContact_WhenAddedToAddressBook_ShouldReturn_false(){
         AddressBook addressBookMain = new AddressBook();
         Contact contact = new Contact("SIYA", "Kapoor", "IUDP", "Amravati", "MH", 444601, 952772864, "siyas@gmail.com");
-        boolean notadded = addressBookMain.addContact(contact);
-        Assertions.assertTrue(notadded);
+        List<Contact> notadded = addressBookMain.addContact(contact);
+        Assertions.assertTrue((BooleanSupplier) notadded);
+    }
+
+    @Test
+    public void givenContactName_whenUpdated_ShouldReturn_true(){
+        AddressBook addressBook = new AddressBook();
+        Contact contact = new Contact("SIYA", "Kapoor", "IUDP", "Amravati", "MH", 444601, 952772864, "siyas@gmail.com");
+        List<Contact> contactList = addressBook.addContact(contact);
+        Contact editContact = addressBook.editContact(contactList, "SIYA", "address","Civil Line");
+        Assertions.assertEquals("Civil Line", editContact.address);
+    }
+
+    @Test
+    public void givenContactName_whenUpdated_ShouldReturn_false(){
+        AddressBook addressBook = new AddressBook();
+        Contact contact = new Contact("SIYA", "Kapoor", "IUDP", "Amravati", "MH", 444601, 952772864, "siyas@gmail.com");
+        List<Contact> contactList = addressBook.addContact(contact);
+        Contact editContact = addressBook.editContact(contactList, "SIYA", "address","Chandni Chowk");
+        Assertions.assertEquals("Civil Line", editContact.address);
     }
 }
